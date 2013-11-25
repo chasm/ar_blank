69592841f07c50542423fe75adc09de519af774e
chasm <charles.munat@gmail.com>
2013-11-25 18:30:08 +0800

    Moved templates into templates directory and renamed/recompiled with -k each.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require handlebars.runtime
-//= require users.template
-//= require user.template
-//= require user_edit.template
-//= require posts.template
+//= require_tree ./templates/
 //= require index
